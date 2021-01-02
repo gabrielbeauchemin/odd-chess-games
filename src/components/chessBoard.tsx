e1d03aa5575010e8a2946dab737e400293c08b39
Gabriel Beauchemin <gabybeauchemin@gmail.com>
2021-01-01 22:57:37 -0500

    initial commit- beginning of board and top nav

---
+++ src/components/chessBoard.tsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+// import Chessground from 'react-chessground'
+// import 'react-chessground/dist/styles/chessground.css'
+
+// class ChessBoard extends React.Component {
+//   render () {
+//     return <Chessground />
+//   }
+// }
+export {};
